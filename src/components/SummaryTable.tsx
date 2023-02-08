08a91ce5c29713aeac2b69af595cca05f8cdc1fa
MikaelRagna <mikael.previtera@gmail.com>
2023-02-08 16:03:34 -0300

    second commit

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -14,27 +14,32 @@ const weekDays = [
 const summaryDates = generateDatesFromYearBeginning();
 
 const minimumSummaryDatesSize = 18 * 7;
-const  amountOfDaysToFall = minimumSummaryDatesSize - summaryDates.length
+const amountOfDaysToFall = minimumSummaryDatesSize - summaryDates.length
 
 export function SummaryTable() {
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
         {weekDays.map((weekDay, i) => {
-          return(
+          return (
             <div key={`${weekDay}-${i}`} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
-            {weekDay}
-          </div>
+              {weekDay}
+            </div>
           )
         })}
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map(date =>{
-          return <HabitDay key={date.toString()}/>
+        {summaryDates.map(date => {
+          return (
+            <HabitDay
+              key={date.toString()}
+              amount={5}
+              completed={Math.round(Math.random() * 5)} />
+          )
         })}
 
-        {amountOfDaysToFall > 0 && Array.from({length: amountOfDaysToFall}).map((_, i) =>{
+        {amountOfDaysToFall > 0 && Array.from({ length: amountOfDaysToFall }).map((_, i) => {
           return <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
         })}
       </div>
